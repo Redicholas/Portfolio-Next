7f54a0b028827165e878f15995c98d259dbb6d78
Nicholas Peters <peters.nicholas@gmail.com>
2023-10-18 10:40:55 +0200

    add base contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,27 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-4xl">Contact</h1>
+      <div className="flex justify-center my-12">
+        <Link href="mailto:peters.nicholas@gmail.com">
+          <i className="fa-solid fa-envelope p-4 text-6xl hover:text-slate-400"></i>
+        </Link>
+        <Link href="https://github.com/Redicholas">
+          <i className="fa-brands fa-github p-4 text-6xl hover:text-slate-400"></i>
+        </Link>
+        <Link href="https://www.linkedin.com/in/nicholas-peters-dev">
+          <i className="fa-brands fa-linkedin p-4 text-6xl hover:text-slate-400"></i>
+        </Link>
+        <Link href="https://goo.gl/maps/ejcEqp13YJfBaLH86">
+          <i className="fa-solid fa-location-dot p-4 text-6xl hover:text-slate-400"></i>
+        </Link>
+      </div>
+      <p className="text-center text-2xl">peters.nicholas@gmail.com</p>
+    </div>
+  );
 };
 
 export default Page;
